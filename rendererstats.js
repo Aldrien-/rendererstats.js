144db8da852e5ad444ffbde516e583bb80754760
Alexandre Lemire <mail@alexandre-lemire.com>
2014-01-17 15:35:40 +0100

    Make it compliant with Vtreem projects.

--- rendererstats.js
+++ rendererstats.js
@@ -1,56 +1,43 @@
 /**
- * @author mrdoob / http://mrdoob.com/
- * @author jetienne / http://jetienne.com/
- */
-/** @namespace */
-var THREEx	= THREEx || {}
-
-/**
  * provide info on THREE.WebGLRenderer
- * 
- * @param {Object} renderer the renderer to update
- * @param {Object} Camera the camera to update
-*/
-THREEx.RendererStats	= function (){
+ */
+RendererStats = function () {
+	var msMin = 100;
+	var msMax = 0;
 
-	var msMin	= 100;
-	var msMax	= 0;
-
-	var container	= document.createElement( 'div' );
+	var container = document.createElement( 'div' );
 	container.style.cssText = 'width:80px;opacity:0.9;cursor:pointer';
 
-	var msDiv	= document.createElement( 'div' );
+	var msDiv = document.createElement( 'div' );
 	msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#200;';
-	container.appendChild( msDiv );
+	container.appendChild(msDiv);
 
-	var msText	= document.createElement( 'div' );
+	var msText = document.createElement( 'div' );
 	msText.style.cssText = 'color:#f00;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-	msText.innerHTML= 'WebGLRenderer';
-	msDiv.appendChild( msText );
+	msText.innerHTML = 'WebGLRenderer';
+	msDiv.appendChild(msText);
 	
 	var msTexts	= [];
-	var nLines	= 9;
-	for(var i = 0; i < nLines; i++){
-		msTexts[i]	= document.createElement( 'div' );
+	var nLines = 9;
+	for(var i = 0; i < nLines; i++)
+	{
+		msTexts[i] = document.createElement( 'div' );
 		msTexts[i].style.cssText = 'color:#f00;background-color:#311;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-		msDiv.appendChild( msTexts[i] );		
-		msTexts[i].innerHTML= '-';
+		msDiv.appendChild(msTexts[i]);		
+		msTexts[i].innerHTML = '-';
 	}
 
+	var lastTime = Date.now();
 
-	var lastTime	= Date.now();
 	return {
-		domElement: container,
+		domElement : container,
 
-		update: function(webGLRenderer){
-			// sanity check
-			console.assert(webGLRenderer instanceof THREE.WebGLRenderer)
-
+		update : function(webGLRenderer) {
 			// refresh only 30time per second
-			if( Date.now() - lastTime < 1000/30 )	return;
-			lastTime	= Date.now()
+			if(Date.now() - lastTime < 1000/30)	return;
+			lastTime = Date.now()
 
-			var i	= 0;
+			var i = 0;
 			msTexts[i++].textContent = "== Memory =====";
 			msTexts[i++].textContent = "Programs: "	+ webGLRenderer.info.memory.programs;
 			msTexts[i++].textContent = "Geometries: "+webGLRenderer.info.memory.geometries;
